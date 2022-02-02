ce2f8087213b6b079c33bdbb0e1a5796f22e25d9
ashukla456.as@gmail.com <ashukla456.as@gmail.com>
2022-02-02 11:16:17 +0530

    Home restaurantItem button added

--- components/home/RestaurantItem.js
+++ components/home/RestaurantItem.js
@@ -6,118 +6,339 @@ import {
   TouchableOpacity,
   FlatList,
   Alert,
+  StyleSheet,
 } from "react-native";
 import MaterialCommunityIcons from "react-native-vector-icons/Ionicons";
 import { Entypo } from "@expo/vector-icons";
 import { FontAwesome } from "@expo/vector-icons";
 import firebase from "../../firebase";
-import AppLoading from "expo-app-loading";
-import useFonts from "../hooks/useFonts";
+import { useDispatch } from "react-redux";
 
-const fetchFont = async () => {
-  await useFonts();
+// export const localRestaurants = [
+//   {
+//     name: "Beachside Bar",
+//     image_url:
+//       "https://cdn.pixabay.com/photo/2016/03/05/19/02/hamburger-1238246__480.jpg",
+//     categories: ["Cafe", "Bar"],
+//     price: "$$",
+//     reviews: 1244,
+//     rating: 4.5,
+//   },
+//   {
+//     name: "Benihana",
+//     image_url:
+//       "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGludGVyaW9yfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+//     categories: ["Cafe", "Bar"],
+//     price: "$$",
+//     reviews: 1244,
+//     rating: 3.7,
+//   },
+//   {
+//     name: "India's Grill",
+//     image_url:
+//       "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGludGVyaW9yfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+//     categories: ["Indian", "Bar"],
+//     price: "$$",
+//     reviews: 700,
+//     rating: 4.9,
+//   },
+//   {
+//     name: "India's Grill",
+//     image_url:
+//       "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGludGVyaW9yfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+//     categories: ["Indian", "Bar"],
+//     price: "$$",
+//     reviews: 700,
+//     rating: 4.9,
+//   },
+//   {
+//     name: "Benihana",
+//     image_url:
+//       "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGludGVyaW9yfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+//     categories: ["Cafe", "Bar"],
+//     price: "$$",
+//     reviews: 1244,
+//     rating: 3.7,
+//     key: 5,
+//   },
+// ];
+
+const RestaurantInfo = (props) => {
+  const [quantity, setQuantity] = useState(0);
+  const dispatch = useDispatch();
+
+  const selectItem = (item, quantity) =>
+    dispatch({
+      type: "ADD_TO_CART",
+      payload: {
+        ...item,
+        items: [...item, { item, quantity }],
+        checkboxValue: true,
+      },
+    });
+
+  //selectedItems: { items: [{item: "", quantity: 0}], restaurantName: "" },
+
+  const addToCart = () => {
+    setQuantity(quantity + 1);
+    selectItem(item, quantity);
+  };
+
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginTop: 10,
+        paddingHorizontal: 15,
+        paddingBottom: 15,
+      }}
+    >
+      <View style={{ width: "70%" }}>
+        <Text style={{ fontSize: 15, fontFamily: "Product-Sans-Bold" }}>
+          {props.name}
+        </Text>
+        <Text
+          style={{
+            fontSize: 13,
+            fontFamily: "Product-Sans-Bold",
+            width: "100%",
+            color: "#79767D",
+          }}
+          numberOfLines={1}
+        >
+          {props.detail}
+        </Text>
+        <Text
+          style={{
+            fontSize: 15,
+            color: "#000",
+            fontFamily: "Product-Sans-Bold",
+          }}
+        >
+          {props.price}
+        </Text>
+      </View>
+      <View
+        style={{
+          // backgroundColor: "#000",
+          // height: 25,
+          // width: 48,
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: 15,
+          flexDirection: "row",
+        }}
+      >
+        {/* <Text style={{ color: "#fff", alignItems: "center" }}>
+          {props.rating}
+        </Text>
+        <FontAwesome name="star" size={10} color="#fff" /> */}
+        {quantity === 0 ? (
+          <TouchableOpacity
+            style={{
+              width: 80,
+              height: 40,
+              backgroundColor: "#000",
+              justifyContent: "center",
+              alignItems: "center",
+              borderRadius: 20,
+            }}
+            onPress={() => setQuantity(quantity + 1)}
+          >
+            <Text
+              style={{
+                fontFamily: "Product-Sans-Bold",
+                color: "#fff",
+                fontSize: 15,
+              }}
+            >
+              Add +
+            </Text>
+          </TouchableOpacity>
+        ) : (
+          <View
+            style={{
+              // backgroundColor: "red",
+              width: 80,
+              height: 33,
+              borderWidth: 0.8,
+              borderRadius: 10,
+              flexDirection: "row",
+              justifyContent: "space-around",
+              margin: 5,
+            }}
+          >
+            <TouchableOpacity
+              onPress={() => setQuantity(quantity - 1)}
+              style={{
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Entypo
+                name="minus"
+                size={16}
+                color="black"
+                style={{ alignItems: "center" }}
+              />
+            </TouchableOpacity>
+            <View style={{ justifyContent: "center" }}>
+              <Text style={{ fontSize: 16, fontFamily: "Product-Sans-Bold" }}>
+                {quantity}
+              </Text>
+            </View>
+            <TouchableOpacity
+              onPress={() => setQuantity(quantity + 1)}
+              style={{
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Entypo
+                name="plus"
+                size={15}
+                color="black"
+                style={{ alignItems: "center" }}
+              />
+            </TouchableOpacity>
+          </View>
+        )}
+      </View>
+    </View>
+  );
 };
 
-export const localRestaurants = [
-  {
-    name: "Beachside Bar",
-    image_url:
-      "https://cdn.pixabay.com/photo/2016/03/05/19/02/hamburger-1238246__480.jpg",
-    categories: ["Cafe", "Bar"],
-    price: "$$",
-    reviews: 1244,
-    rating: 4.5,
+const RestaurantImage = (props) => {
+  const [heart, setHeart] = useState(false);
+
+  const Heart = () => {
+    return heart ? (
+      <Entypo name="heart" size={25} color="#FF4242" />
+    ) : (
+      <Entypo name="heart-outlined" size={25} color="#fff" />
+    );
+  };
+
+  const Type = () => {
+    return props.type === "veg" ? (
+      <Image
+        source={{
+          uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Indian-vegetarian-mark.svg/640px-Indian-vegetarian-mark.svg.png",
+        }}
+        resizeMode="contain"
+        style={{ height: 18, width: 18 }}
+      />
+    ) : (
+      <Image
+        source={{
+          uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Indian-vegetarian-mark.svg/640px-Indian-vegetarian-mark.svg.png",
+        }}
+        resizeMode="contain"
+        style={{ height: 18, width: 18, tintColor: "maroon" }}
+      />
+    );
+  };
+
+  return (
+    <>
+      <Image
+        source={{
+          uri: props.image,
+        }}
+        style={{ width: "100%", height: 200, borderRadius: 10 }}
+      />
+      <TouchableOpacity
+        activeOpacity={0.1}
+        style={{ position: "absolute", right: 10, top: 10 }}
+        onPress={() => (heart ? setHeart(false) : setHeart(true))}
+      >
+        <Heart />
+      </TouchableOpacity>
+      <View style={[styles.typeBox]}>
+        <Type />
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  typeBox: {
+    left: 10,
+    top: 10,
+
+    position: "absolute",
+    elevation: 30,
   },
-  {
-    name: "Benihana",
-    image_url:
-      "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGludGVyaW9yfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
-    categories: ["Cafe", "Bar"],
-    price: "$$",
-    reviews: 1244,
-    rating: 3.7,
-  },
-  {
-    name: "India's Grill",
-    image_url:
-      "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGludGVyaW9yfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
-    categories: ["Indian", "Bar"],
-    price: "$$",
-    reviews: 700,
-    rating: 4.9,
-  },
-  {
-    name: "India's Grill",
-    image_url:
-      "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGludGVyaW9yfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
-    categories: ["Indian", "Bar"],
-    price: "$$",
-    reviews: 700,
-    rating: 4.9,
-  },
-  {
-    name: "Benihana",
-    image_url:
-      "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGludGVyaW9yfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
-    categories: ["Cafe", "Bar"],
-    price: "$$",
-    reviews: 1244,
-    rating: 3.7,
-    key: 5,
-  },
-];
+});
 
 export default function RestaurantItem({ navigation, ...props }) {
   const [restaurant, setRestaurant] = useState([
     {
-      name: "Veg Classic Burger",
+      name: "Veg Crisp Burger",
       image_url:
         "https://images.pexels.com/photos/3616956/pexels-photo-3616956.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
       categories: ["Cafe", "Bar"],
+      detail:
+        "Too Hot to handle, veg crispy burger is served you with the best crisp possible melts as soon as it enters in your mouth",
       price: "₹ 150",
       reviews: 1244,
       rating: 4.5,
       key: 1,
+      type: "veg",
     },
     {
       name: "Cheese Burger",
       image_url:
         "https://cdn.pixabay.com/photo/2016/03/05/19/02/hamburger-1238246__480.jpg",
       categories: ["Cafe", "Bar"],
+      detail:
+        "Too Hot to handle, veg crispy burger is served you with the best crisp possible melts as soon as it enters in your mouth",
       price: "₹ 200",
       reviews: 1244,
       rating: 3.7,
       key: 2,
+      type: "veg",
     },
     {
       name: "Chinese Noodles",
       image_url:
         "https://images.pexels.com/photos/2098135/pexels-photo-2098135.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
       categories: ["Indian", "Bar"],
+      detail:
+        "Too Hot to handle, veg crispy burger is served you with the best crisp possible melts as soon as it enters in your mouth",
       price: "₹ 350",
       reviews: 700,
       rating: 4.9,
       key: 3,
+      type: "veg",
     },
     {
       name: "India's Grill",
       image_url:
         "https://b.zmtcdn.com/data/pictures/chains/0/18798800/e390c1d8f4df7d86b3787fe98ecd4427.jpg?fit=around|300:273&crop=300:273;*,*",
       categories: ["Indian", "Bar"],
+      detail:
+        "Too Hot to handle, veg crispy burger is served you with the best crisp possible melts as soon as it enters in your mouth",
       price: "₹ 450",
       reviews: 700,
       rating: 4.9,
       key: 4,
+      type: "non-veg",
     },
     {
       name: "Momos",
       image_url:
         "https://b.zmtcdn.com/data/pictures/chains/3/18689923/fe92135685160e19b3a464a788876e45.jpg?output-format=webp&fit=around|771.75:416.25&crop=771.75:416.25;*,*",
       categories: ["Cafe", "Bar"],
+      detail:
+        "Too Hot to handle, veg crispy burger is served you with the best crisp possible melts as soon as it enters in your mouth",
       price: "₹ 350",
       reviews: 1244,
       rating: 3.7,
       key: 5,
+      type: "veg",
     },
   ]);
 
@@ -176,11 +397,12 @@ export default function RestaurantItem({ navigation, ...props }) {
                     marginHorizontal: "2.5%",
                   }}
                 >
-                  <RestaurantImage image={item.image_url} />
+                  <RestaurantImage image={item.image_url} type={item.type} />
                   <RestaurantInfo
                     name={item.name}
                     price={item.price}
                     rating={item.rating}
+                    detail={item.detail}
                   />
                 </View>
               </TouchableOpacity>
@@ -216,76 +438,3 @@ export default function RestaurantItem({ navigation, ...props }) {
     </>
   );
 }
-
-const RestaurantImage = (props) => {
-  const [heart, setHeart] = useState(false);
-
-  const Heart = () => {
-    return heart ? (
-      <Entypo name="heart" size={30} color="#FF4242" />
-    ) : (
-      <Entypo name="heart-outlined" size={20} color="#fff" />
-    );
-  };
-
-  return (
-    <>
-      <Image
-        source={{
-          uri: props.image,
-        }}
-        style={{ width: "100%", height: 200, borderRadius: 10 }}
-      />
-      <TouchableOpacity
-        style={{ position: "absolute", right: 10, top: 10 }}
-        onPress={() => (heart ? setHeart(false) : setHeart(true))}
-      >
-        <Heart />
-      </TouchableOpacity>
-    </>
-  );
-};
-
-const RestaurantInfo = (props) => (
-  <View
-    style={{
-      flexDirection: "row",
-      justifyContent: "space-between",
-      alignItems: "center",
-      marginTop: 10,
-      paddingHorizontal: 15,
-      paddingBottom: 15,
-    }}
-  >
-    <View>
-      <Text style={{ fontSize: 15, fontFamily: "Product-Sans-Bold" }}>
-        {props.name}
-      </Text>
-      <Text
-        style={{
-          fontSize: 13,
-          color: "gray",
-          fontFamily: "Product-Sans-Regular",
-        }}
-      >
-        {props.price}
-      </Text>
-    </View>
-    <View
-      style={{
-        backgroundColor: "#000",
-        height: 25,
-        width: 48,
-        alignItems: "center",
-        justifyContent: "center",
-        borderRadius: 15,
-        flexDirection: "row",
-      }}
-    >
-      <Text style={{ color: "#fff", alignItems: "center" }}>
-        {props.rating}{" "}
-      </Text>
-      <FontAwesome name="star" size={10} color="#fff" />
-    </View>
-  </View>
-);
--- screens/OrderHistory.js
+++ screens/OrderHistory.js
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import BottomTab from "../components/home/BottomTab";
 import { Divider } from "react-native-elements/dist/divider/Divider";
@@ -11,9 +11,24 @@ export default function OrderHistory({ navigation }) {
         <Text style={{ fontFamily: "Product-Sans-Italic", fontSize: 20 }}>
           Order History
         </Text>
+        <View style={styles.viewBox}></View>
       </View>
       <Divider width={1} />
       <BottomTab navigation={navigation} />
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  viewBox: {
+    height: 100,
+    width: 100,
+    backgroundColor: "teal",
+    elevation: 60,
+  },
+});
+
+const shadow = {
+  shadowOpacity: 1,
+  shadowRadius: 20,
+};
